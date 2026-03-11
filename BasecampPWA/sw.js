530fbfc5cf83ad004f460042a6f54ca9e6188a93
bradlarge-bc <large.brad@gmail.com>
2026-03-11 10:59:19 -0400

    Add files via upload

---
+++ BasecampPWA/sw.js
@@ -0,0 +1,29 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2868
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 const CACHE_NAME = 'devo-cache-v1';\
+const ASSETS = [\
+  '/',\
+  '/index.html',\
+  '/manifest.json'\
+];\
+\
+// Install Service Worker\
+self.addEventListener('install', (e) => \{\
+  e.waitUntil(\
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))\
+  );\
+\});\
+\
+// Fetching content\
+self.addEventListener('fetch', (e) => \{\
+  e.respondWith(\
+    caches.match(e.request).then((response) => \{\
+      return response || fetch(e.request);\
+    \})\
+  );\
+\});}
